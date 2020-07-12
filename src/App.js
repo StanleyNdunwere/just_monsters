fc7e6ac261ebc5824f6d9e28c0f9af7d057c68c8
Stanley Ndunwere <StanleyNdunwere@gmail.com>
2020-07-12 15:31:24 +0100

    started dynamic content

--- src/App.js
+++ src/App.js
@@ -11,26 +11,31 @@ class App extends Component {
     this.state = {
       firstName: "First Name is Stanley",
       lastName: "Last Name is Stanley",
-      middleName: "Middle Name is Stanley"
+      middleName: "Middle Name is Stanley",
+      monsters: [
+        {
+          name: "Jerome Boateng",
+          id: "1"
+        },
+        {
+          name: "Ivan Rakitic",
+          id: "1"
+        },
+        {
+          name: "Raheem Stirling",
+          id: "1"
+        },
+      ],
     }
   }
   render() {
     return (
       <div className="App">
         <header className="App-header" >
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <h2>{this.state.firstName}</h2>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {
+            this.state.monsters.map((monster) => <h1>{monster.name}</h1>)
+          }
           <button onClick={() => this.setState({ firstName: "Jerome" })}>
             This is arrow button
           </button>
